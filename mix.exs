41fac6a11418490e3f324bd64c08125cc24f42e2
Faheetah <Faheetah@users.noreply.github.com>
2021-09-15 21:43:18 -0500

    Version bump for DLC, make DLC not need to be downloaded

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CombatLog.MixProject do
   def project do
     [
       app: :combat_log,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
